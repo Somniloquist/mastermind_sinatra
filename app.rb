fe35a2b80a577e771781e061a492209bda8e0670
Robin Unger <robin.e.unger@gmail.com>
2019-08-21 18:43:37 -0500

    push guesses to decoding grid

--- app.rb
+++ app.rb
@@ -1,16 +1,15 @@
 class Mastermind < Sinatra::Base
   configure do
-    set :colors => {
-      1 => "red",
-      2 => "green",
-      3 => "blue",
-      4 => "yellow",
-      5 => "brown",
-      6 => "orange",
-      7 => "black",
-      8 => "white"
-    }
     # enable secure sessions
+  end
+
+  def add_guess_to_board(params)
+    guess = []
+    session[:game].code_length.times do |count|
+      guess << params["color-#{count+1}"] unless params["color-#{count+1}"].nil?
+    end
+
+    guess.map { |color| Cell.new(color) }
   end
 
   configure :development do
@@ -22,16 +21,20 @@ class Mastermind < Sinatra::Base
   get '/' do
     @session = session
     @game = session[:game]
-    erb :index
+
+    erb :index 
   end
 
   post '/' do
-    turns = params['turns'].to_i
-    player = Player.new(role: params['role'].to_i)
-    game = Game.new(turns: turns, player: player)
-
-    # store the game
-    session[:game] = game
+    if session[:game]
+      guess = add_guess_to_board(params)
+      session[:game].push_to_decoding_grid(guess)
+      params.clear
+    else
+      turns = params['turns'].to_i
+      player = Player.new(role: params['role'].to_i)
+      session[:game] = Game.new(turns: turns, player: player)
+    end
 
     redirect '/'
   end
